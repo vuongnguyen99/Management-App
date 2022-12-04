7cd73bd297566b22aae1feafedf2de6df49a9562
vuongnguyen99 <nqvuong7799@gmail.com>
2022-12-04 21:21:42 +0700

    change source code

---
+++ Management-Common/Exception/NotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management_Common.Exception
+{
+    public class NotFoundException: SystemException
+    {
+        public NotFoundException()
+        {
+
+        }
+        public NotFoundException(string message): base(message)
+        {
+
+        }
+        public NotFoundException(string message, SystemException inner): base(message, inner)
+        {
+
+        }
+    }
+}
